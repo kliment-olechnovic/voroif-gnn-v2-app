c9f0be185b7cf86025c18fe8629dac32c902ee72
kliment-olechnovic <kliment.olechnovic@gmail.com>
2025-06-19 20:40:01 +0200

    Improbed dependency building script.

--- build-voronota-js.bash
+++ build-voronota-js.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SCRIPTDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 g++ -std=c++14 -I"./voronota-js-src/expansion_js/src/dependencies" -O3 -o "./voronota-js" $(find ./voronota-js-src/ -name '*.cpp')
